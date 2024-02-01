97781566daf37bc715669efc1644a89aa55d24c2
Carlos <carlos.scherer@discente.ufg.br>
2024-02-01 19:47:55 -0300

    Funções básicas de setup

--- pac.c
+++ pac.c
@@ -2,28 +2,25 @@
 #include <time.h>
 #include <stdlib.h>
 
+//Aloca um baralho de cartas dinamicamente e salva todas em um vetor estático
 void novoBaralho(carta *baralho[52]){
     srand(time(NULL));
     
-    int c=-1;
-
     for(int i = 0; i < 52; i++){
-
         baralho[i] = (carta *) calloc(1, sizeof(carta));
-        
-        if(!(i%13)) c++;
         baralho[i]->valor = i%13 + 1;
-        if(c==0) baralho[i]->naipe = 'c';
-        if(c==1) baralho[i]->naipe = 'e';
-        if(c==2) baralho[i]->naipe = 'o';
-        if(c==3) baralho[i]->naipe = 'p';
+
+        if(i < 13) baralho[i]->naipe = 'c';
+        else if(i < 26) baralho[i]->naipe = 'e';
+        else if(i < 39) baralho[i]->naipe = 'o';
+        else baralho[i]->naipe = 'p';
+
         baralho[i]->id = rand();
     }
-    
 }
 
+//Embaralha as cartas pelo id aleatório
 void embaralha(carta *baralho[52]){
-
     for(int i = 0; i < 51; i++)
         for(int j = 0; j < 51-i; j++)
             if(baralho[j]->id > baralho[j+1]->id){
@@ -31,23 +28,52 @@ void embaralha(carta *baralho[52]){
                 baralho[j] = baralho[j+1];
                 baralho[j+1] = aux;
             }
-
 }
 
-void novoDeck(){
-
+//Coloca a quantidade certa de cartas em cada coluna e seta a última carta de cada coluna como virada
+void setColunas(carta *baralho[52], lista colunas[7]){
+    int c=0;
+    for(int i = 0; i < 7; i++){
+        colunas[i].comeco=0;
+        colunas[i].tamanho=i+1;
+        for(int j = 0; j <= i; j++){
+            baralho[c]->proxima = colunas[i].comeco;
+            colunas[i].comeco = baralho[c];
+            if(j==0) baralho[c]->virada = 1;
+            c++;
+        }
+    }
 }
 
-void fundacao(){
-
+//Coloca o restante das cartas no deck
+void setDeck(carta *baralho[52], lista *deck){
+    deck->comeco = 0;
+    deck->tamanho = 24;
+    for(int i=28; i<52; i++){
+        baralho[i]->proxima = deck->comeco;
+        deck->comeco = baralho[i];
+    }
 }
 
-void pilhas(){
-    
+//Limpa o lixo de memória nas pilhas
+void setPilhas(lista pilhas[4]){
+    for(int i = 0; i < 4; i++){
+        pilhas[i].comeco = 0;
+        pilhas[i].tamanho = 0;
+    }
 }
 
-void separaCartas(){
+//Setup com o baralho já criado
+void separarCartas(carta *baralho[52], lista *deck, lista colunas[7], lista pilhas[4]){
+    setColunas(baralho, colunas);
+    setDeck(baralho, deck);
+    setPilhas(pilhas);
+}
 
+void liberaBaralho(carta *baralho[52]){
+    for(int i = 0; i < 52; i++){
+        free(baralho[i]);
+    }
 }
 
 void mostrar(){
