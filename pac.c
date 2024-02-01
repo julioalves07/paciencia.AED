c89149027e8b882dfd3d6ba2a3957992eff1485c
Carlos <carlos.scherer@discente.ufg.br>
2024-02-01 16:33:25 -0300

    bubblesort e nomes de variáveis

--- pac.c
+++ pac.c
@@ -2,34 +2,34 @@
 #include <time.h>
 #include <stdlib.h>
 
-void novoBaralho(carta *k[52]){
+void novoBaralho(carta *baralho[52]){
     srand(time(NULL));
     
     int c=-1;
 
     for(int i = 0; i < 52; i++){
 
-        k[i] = (carta *) calloc(1, sizeof(carta));
+        baralho[i] = (carta *) calloc(1, sizeof(carta));
         
         if(!(i%13)) c++;
-        k[i]->valor = i%13 + 1;
-        if(c==0) k[i]->naipe = 'c';
-        if(c==1) k[i]->naipe = 'e';
-        if(c==2) k[i]->naipe = 'o';
-        if(c==3) k[i]->naipe = 'p';
-        k[i]->id = rand();
+        baralho[i]->valor = i%13 + 1;
+        if(c==0) baralho[i]->naipe = 'c';
+        if(c==1) baralho[i]->naipe = 'e';
+        if(c==2) baralho[i]->naipe = 'o';
+        if(c==3) baralho[i]->naipe = 'p';
+        baralho[i]->id = rand();
     }
     
 }
 
-void embaralha(carta *k[52]){
+void embaralha(carta *baralho[52]){
 
-    for(int i = 0; i < 52; i++)
-        for(int j = 0; j < 52; j++)
-            if(k[j]->id > k[i]->id){
-                carta *aux = k[j];
-                k[j] = k[i];
-                k[i] = aux;
+    for(int i = 0; i < 51; i++)
+        for(int j = 0; j < 51-i; j++)
+            if(baralho[j]->id > baralho[j+1]->id){
+                carta *aux = baralho[j];
+                baralho[j] = baralho[j+1];
+                baralho[j+1] = aux;
             }
 
 }
@@ -42,7 +42,7 @@ void fundacao(){
 
 }
 
-void pilhas(carta *baralho[]){
+void pilhas(){
     
 }
 
