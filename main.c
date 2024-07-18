ba25d9f5b15b04bf0f2daf46a842281a523eb5d0
Júlio César Alves <132590104+julioalves07@users.noreply.github.com>
2024-07-18 16:19:14 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,39 +1,16 @@
 #include "pac.h"
 #include <stdio.h>
 
-//teste
-void printcolunas(lista colunas[7]){
-    for(int i=0; i<7; i++){
-        carta * aux = colunas[i].comeco;
-        while(aux){
-            printf("%d%c, ", aux->valor, aux->naipe);
-            aux = aux->proxima;
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-//teste
-void printdeck(lista * deck){
-    carta * aux = deck->comeco;
-    while(aux){
-        printf("%d%c, ", aux->valor, aux->naipe);
-        aux = aux->proxima;
-    }
-    printf("\n");
-}
-
 int main(){
     carta *baralho[52];
-    lista deck, colunas[7], pilhas[4];
+    lista deck, pile, colunas[7], pilhas[4];
 
     novoBaralho(baralho);
     embaralha(baralho);
-    separarCartas(baralho, &deck, colunas, pilhas);
-    //testes
-    printcolunas(colunas);
-    printdeck(&deck);
-    
+    separarCartas(baralho, &deck, &pile, colunas, pilhas);
+
+    ///inicio do jogo
+	while(paciencia(&deck, &pile, colunas, pilhas));
     liberaBaralho(baralho);
     return 0;
 }
