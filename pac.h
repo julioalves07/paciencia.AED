f6d1efc576d204d624579a92e40aaa26fc9833af
Júlio César Alves <132590104+julioalves07@users.noreply.github.com>
2024-07-18 16:18:01 -0300

    Update pac.h

--- pac.h
+++ pac.h
@@ -3,8 +3,8 @@
 
 //Virada serve para dizer se a carta está virada pra cima e é pra ser usada na função de mostrar
 typedef struct _carta {
-    int valor, id;
-    char naipe, virada;
+    int valor, id, virada;
+    char naipe;
     struct _carta *proxima;
 } carta;
 
@@ -18,10 +18,23 @@ void embaralha(carta *baralho[52]);
 void setColunas(carta *baralho[52], lista colunas[7]);
 void setDeck(carta *baralho[52], lista *deck);
 void setPilhas(lista pilhas[4]);
-void separarCartas(carta *baralho[52], lista *deck, lista colunas[7], lista pilhas[4]);
+void separarCartas(carta *baralho[52], lista *deck, lista *pile, lista colunas[7], lista pilhas[4]);
 void liberaBaralho(carta *baralho[52]);
-void mostrar();
-void mover();
-void finaliza();
+
+void mostrar(lista *deck, lista *pile);
+void finaliza(lista pilhas[4]);
+int mover(int posicao, int origem, int destino, lista *pile, lista colunas[7], lista pilhas[4]);
+int movimentoColunas(carta *origem, carta *destino);
+int movimentoFundacao(carta *origem, carta *destino);
+int idPilha(char index);
+
+int comando(lista *deck, lista *pile, lista colunas[7], lista pilhas[4]);
+int paciencia(lista *deck, lista *pile, lista colunas[7], lista pilhas[4]);
+
+void printpaciencia(lista *deck, lista *pile, lista colunas[7], lista pilhas[4]);
+void printpilhas(lista pilhas[4]);
+void pilhainvertida(carta *topo);
+void printdeck(lista *deck, lista *pile);
+void printcolunas(lista colunas[7]);
 
 #endif
